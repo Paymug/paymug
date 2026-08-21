5cf7498cb696f258ecb3bc99a2aa5470b4397485
Hieu Nguyen (Jack) <nc.hieu@live.com>
2026-08-21 18:41:07 +0700

    fix cf deployment

--- src/lib/setup-database-migrations.config.ts
+++ src/lib/setup-database-migrations.config.ts
@@ -43,8 +43,7 @@ export const runtimeDatabaseMigrations: RuntimeDatabaseMigration[] = [
   {
     "name": "0001_primary_store.sql",
     "statements": [
-      "ALTER TABLE `users` ADD `primary_store_id` text;",
-      "UPDATE `users`\nSET `primary_store_id` = `active_store_id`\nWHERE `primary_store_id` IS NULL;"
+      "ALTER TABLE `users` ADD `primary_store_id` text;\nUPDATE `users`\nSET `primary_store_id` = `active_store_id`\nWHERE `primary_store_id` IS NULL;"
     ]
   },
   {
