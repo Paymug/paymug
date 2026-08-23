413a307bff3f6d7227abfcaae70f07f87c7b7ce1
Hieu Nguyen (Jack) <nc.hieu@live.com>
2026-08-23 15:37:01 +0700

    fix cf deployment

--- src/app/dashboard/orders/orders-page.utils.ts
+++ src/app/dashboard/orders/orders-page.utils.ts
@@ -89,7 +89,7 @@ export async function buildDashboardOrderItems(
       environment: order.environment,
       createdAt: order.createdAt,
       paidAt: order.paidAt,
-      paymentFailureDetails: order.paymentFailureDetails,
+      paymentFailureDetails: order.paymentFailureDetails ?? undefined,
       deliveryContent:
         order.status === "paid" ? order.deliveryContent : undefined,
       productFiles: order.status === "paid" ? order.productFiles : [],
